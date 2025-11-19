504446cb3b2d6eb48d386646d2492fc47157cbad
kinu <kinuua3@gmail.com>
2025-11-19 21:37:01 +0900

    ルートページ作りました

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <div><h1>20--年--月--日</h1></div>
+      <div>
+        <div><p>今日のタスクを入力する</p></div>
+        <div><p>タスクを確認する</p></div>
+      </div>
+    </div>
   )
 }
 
