228f822d3f050121153a5a0273fc5ec60b2f571b
taga3s <bepozurapu274@gmail.com>
2025-11-19 21:59:52 +0900

    ルートページのレイアウト用のCSSを追加しました

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import styles from './App.module.css'
+
 function App() {
   return (
-    <div>
-      <div><h1>20--年--月--日</h1></div>
-      <div>
-        <div><p>今日のタスクを入力する</p></div>
-        <div><p>タスクを確認する</p></div>
+    <div className={styles.app}>
+      <div className={styles.container}>
+        <div className={styles.dateHeader}>
+          <span>20--年--月--日</span>
+        </div>
+        <div className={styles.taskContainer}>
+          <button className={styles.taskButton}>
+            <span>今日のタスクを入力する</span>
+          </button>
+          <button className={styles.taskButton}>
+            <span>タスクを確認する</span>
+          </button>
+        </div>
       </div>
     </div>
   )
