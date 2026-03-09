b827cb6e0b3963b392c5c7ce196a53c60a588910
taga3s <bepozurapu274@gmail.com>
2026-03-09 21:57:05 +0900

    feat: use localStorage

---
+++ src/data/repository.ts
@@ -0,0 +1,33 @@
+import { mock } from "./mock";
+import type { Task } from "./types";
+
+export const initStorage = (): typeof mock => {
+  const raw = localStorage.getItem("app-storage");
+  if (!raw) {
+    localStorage.setItem("app-storage", JSON.stringify(mock));
+  }
+  return JSON.parse(localStorage.getItem("app-storage")!);
+};
+
+export const repository = {
+  getTask: (key: string, id: number) => {
+    // TODO
+  },
+  listTasks: (key: string): Task[] => {
+    const raw = localStorage.getItem("app-storage");
+    if (!raw) {
+      return [];
+    }
+    const data = JSON.parse(raw);
+    return data[key]?.tasks || [];
+  },
+  addTask: (key: string, task: Task): void => {
+    // TODO
+  },
+  updateTask: (key: string, task: Task): void => {
+    // TODO
+  },
+  deleteTask: (key: string, id: number): void => {
+    // TODO
+  },
+};
